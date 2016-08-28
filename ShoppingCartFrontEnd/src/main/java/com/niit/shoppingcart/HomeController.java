a739c1bc3bb00c2be2a1ef3196b920574d08347e
muppanikhitha <muppanikhitha@gmail.com>
2016-08-28 19:10:41 +0530

    backend integrate with fronrend

--- ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/HomeController.java
+++ ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/HomeController.java
@@ -1,11 +1,19 @@
 package com.niit.shoppingcart;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.niit.shoppingcart.dao.CategoryDAO;
+import com.niit.shoppingcart.model.Category;
+
 @Controller
 public class HomeController {
+	@Autowired
+	CategoryDAO categoryDAO;
 	
 	
 /*	@RequestMapping("/")
@@ -21,6 +29,9 @@ public class HomeController {
 	public ModelAndView home(){
 		ModelAndView mv=new ModelAndView("Home");
 		mv.addObject("message", "Thank you for visiting this URL");
+		List<Category> categoryList=categoryDAO.list();
+		mv.addObject("categoryList",categoryList);
+		System.out.println("Size:"+categoryList.size());
 		return mv;
 	}
 	@RequestMapping("/Login")
