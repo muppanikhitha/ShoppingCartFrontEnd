ad4022544de99e3477874970de69386a1b415ccf
muppanikhitha <muppanikhitha@gmail.com>
2016-09-27 11:39:12 +0530

    shoppingcartfrontend2

--- ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/CategoryController.java
+++ ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/CategoryController.java
@@ -1,7 +1,6 @@
 package com.niit.shoppingcart;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,71 +8,84 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import com.niit.shoppingcart.dao.CategoryDAO;
 import com.niit.shoppingcart.model.Category;
 
 @Controller
+
 public class CategoryController {
-	private static Logger log=LoggerFactory.getLogger(CategoryController .class);
+
 	@Autowired
-private	CategoryDAO categoryDAO;
+	private Category category;
+
 	@Autowired
-	private	Category category;
-	@RequestMapping(value="/categories",method=RequestMethod.GET)
-	public String listCategories(Model model){
-		log.debug("Starting of the method listCategories");
-		model.addAttribute("category",category);
-		model.addAttribute("categoryList",categoryDAO.list());
-		log.debug("End of the method listCategories");
+	private CategoryDAO categoryDAO;
+
+	@RequestMapping(value = "/category", method = RequestMethod.GET)
+	public String listCategory(Model model) {
+		model.addAttribute("category", category);
+		model.addAttribute("categoryList", this.categoryDAO.list());
 		return "category";
 	}
-	@RequestMapping(value="/category/add",method=RequestMethod.POST)
-	public String addCategory(@ModelAttribute("category")Category category){
-	log.debug("Starting of the method addCategory");
-	categoryDAO.save(category);
-	
-	log.debug("Ending of the method addCategory");
-	return "category";
-	}
-	@RequestMapping("category/remove/{id}")
-	public ModelAndView deleteCategory(@PathVariable("id")Category id) throws Exception{
-	boolean flag=categoryDAO.delete(id);
-	ModelAndView mv=new ModelAndView( "category");
-	String message="Successfully done the operation";
-	if(flag!=true)
-	{
-		message="The operation could not success";
-	}
-	mv.addObject("message", message);
-	return mv;
-	}
-	
-	@RequestMapping("category/edit/{id}")
-	public ModelAndView editCategory(@ModelAttribute("category")Category category) {
-		ModelAndView mv=new ModelAndView();
-		if(categoryDAO.get(category.getId())!=null)
-		{
-			categoryDAO.update(category);
-			mv.addObject("messge","Successfuly updated");
-		}
-		else{
-			mv.addObject("errormessage","Could update the record");
-		}
-		return mv;
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+	@RequestMapping(value = "category/add", method = RequestMethod.POST)
+	public String addCategory(@ModelAttribute("category") Category category) {
+		//check if the record exist with this id
+		//if exist, display error message to the admin record already  exist
+		//else save the record
+		
+		ModelAndView mv = new ModelAndView("/category");
+		if (categoryDAO.get(category.getId()) == null) {
+			categoryDAO.save(category);
+			System.out.println("saved");
+			mv.addObject("SavedMsg", "saved successfully");
+		} else {
+			mv.addObject("errorMessage", "The record exist with this id"
+					+ category.getId());
+		}
+		return "redirect:/category";
+
+	}
+
+	@RequestMapping(value="category/Update/{id}")
+	public String updateCategory(@PathVariable("id") Category category) {
+		//check whether category exist with this id?
+		//if exists, update the existing category
+		//if doesnot exist display error message
+		System.out.println("i am in update method");
+		System.out.println(category.getId());
+		/*category=categoryDAO.get(id);*/
+		ModelAndView mv = new ModelAndView("/category");
+		
+/*		mv.addObject("clickedEdit", true);*/
+		if (categoryDAO.get(category.getId()) != null) {
+			categoryDAO.update(category);
+			mv.addObject("updateMsg", "successfully updated");
+		} else {
+			mv.addObject("ErrorUpdateMsg", "couldnot update the record");
+		}
+		return "redirect:/category";
+	}
+
+	@RequestMapping(value="category/Remove/{id}")
+	public String deleteCategory(@PathVariable("id") String id)
+			throws Exception {
+		//if id exist in category delete it
+		//else display error message
+		System.out.println(id);
+		System.out.println("i am in delete method");
+		category=categoryDAO.get(id);
+		ModelAndView mv = new ModelAndView("category");
+	    
+		boolean flag = categoryDAO.delete(category);
+		String msg = "The operation is successfully done";
+		if (flag == false) {
+			msg = "The operation  could not success";
+		}
+		mv.addObject("msg", msg);
+		return "redirect:/category";
+	}
 }
--- ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/HomeController.java
+++ ShoppingCartFrontEnd/src/main/java/com/niit/shoppingcart/HomeController.java
@@ -4,57 +4,74 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.niit.shoppingcart.dao.CategoryDAO;
+import com.niit.shoppingcart.dao.UserDetailsDAO;
 import com.niit.shoppingcart.model.Category;
+import com.niit.shoppingcart.model.UserDetails;
 
 @Controller
 public class HomeController {
+
+	/*
+	 * @RequestMapping("/") public String homePage(){ return "Home" }
+	 */
+
 	@Autowired
+	// to inject the object in the home controller automatically
 	CategoryDAO categoryDAO;
 	
+	@Autowired
+	UserDetailsDAO userDetailsDAO;
 	
-/*	@RequestMapping("/")
-	public String ShoppingCartHomePage()
-	{
-		return"Home";
-	}
-*/
-	
-	
-	
+	@Autowired
+	UserDetails userDetails;
+
 	@RequestMapping("/")
-	public ModelAndView home(){
-		ModelAndView mv=new ModelAndView("Home");
-		mv.addObject("message", "Thank you for visiting this URL");
-		List<Category> categoryList=categoryDAO.list();
-		mv.addObject("categoryList",categoryList);
-		System.out.println("Size:"+categoryList.size());
-		return mv;
-	}
-	@RequestMapping("/Login")
-	public ModelAndView Login()
-	{
-		ModelAndView mv=new ModelAndView("Login");
-		mv.addObject("UserClickedLoginHere", "true");
-
-		return mv;
-
-
-	}
-	
-	@RequestMapping("/Register")
-	public ModelAndView Register(){
-		ModelAndView mv=new ModelAndView("Registration");
-		mv.addObject("UserClickedRegisterHere", "true");
-
-
-		return mv;
-
+	public ModelAndView home() {
+		ModelAndView m = new ModelAndView("Home");
 		
+		// get all categories along with products
+		// you have to integrate with the backend project
+		List<Category> categoryList = categoryDAO.list();
+		m.addObject("categoryList", categoryList);
+		return m;
 	}
 	
-	
+	@RequestMapping(value="register", method=RequestMethod.POST)
+	public ModelAndView registerUser(@ModelAttribute("userDetails") UserDetails userDetails) {
+		
+		ModelAndView mv = new ModelAndView("/Home");
+		//if(userDetailsDAO.get(userDetails.getId())==null){
+			userDetailsDAO.save(userDetails);
+			mv.addObject("SuccessMessage","You are successfully register");
+		//}
+		//else{
+		//	mv.addObject("failureMessage","user exist  with  this id");
+		//}
+		return mv;
 	}
+	
+	@RequestMapping("/Registration")
+	public ModelAndView register() {
+		ModelAndView mv = new ModelAndView("/Home");
+		mv.addObject("RegisterMsg", "you are in register page");
+		mv.addObject("userDetails", userDetails);
+		mv.addObject("userClickedRegisterHere", true);
+		return mv;
+	}
+
+	@RequestMapping("/Login")
+	public ModelAndView Login() {
+		ModelAndView m = new ModelAndView("/Home");
+		m.addObject("LoginMsg", "you are in login page");
+		m.addObject("userDetails", userDetails);
+		m.addObject("userClickedLoginHere", true);
+		return m;
+	}
+
+}
